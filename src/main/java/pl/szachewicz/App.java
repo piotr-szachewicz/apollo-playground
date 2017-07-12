cc79d31d795e6d6bd1aed5fd479d78843933479c
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2017-07-12 21:50:36 +0200

    Created an empty Maven project.
    Project generated using the following command:
    
    mvn -B archetype:generate \
      -DarchetypeGroupId=org.apache.maven.archetypes \
      -DgroupId=pl.szachewicz \
      -DartifactId=apollo-playground

---
+++ src/main/java/pl/szachewicz/App.java
@@ -0,0 +1,13 @@
+package pl.szachewicz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
